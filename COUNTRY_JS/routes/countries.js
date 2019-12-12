71dcda429276f6e750dad1670438cf5c2f1fd819
pashacaclaudia <pashaca.claudia.carolina@iisgalvanimi.edu.it>
2019-12-12 13:55:00 +0000

    3.2 Prima parte

---
+++ COUNTRY_JS/routes/countries.js
@@ -0,0 +1,11 @@
+var express = require('express'); //Carica il modulo express
+var router = express.Router(); //Ottiene un oggetto router
+var country = require('countryjs');
+
+router.get('/info/:state/', function(req, res, next){ //Rendiamo lo stato un parametro
+ res.send(req.params);
+})
+
+module.exports = router;
+
+
